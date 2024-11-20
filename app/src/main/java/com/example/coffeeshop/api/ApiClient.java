bf0d49e4e52ae44e28ed3058326700a185465cdf
nicole <nickmendess@hotmail.com>
2024-11-20 20:35:01 -0300

    render-teste

--- app/src/main/java/com/example/coffeeshop/api/ApiClient.java
+++ app/src/main/java/com/example/coffeeshop/api/ApiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "http://192.168.1.106:8000/";  // Para o Android Emulator
+    private static final String BASE_URL = "http://192.168.1.1:5000/";  // Para o Android Emulator
     private static Retrofit retrofit;
 
     public static Retrofit getRetrofitInstance() {
