701fcaefeb1ad2386c3e890bc1da4acfafa51f30
nicole <nickmendess@hotmail.com>
2024-11-21 23:27:44 -0300

    cart funcionando

--- app/src/main/java/com/example/coffeeshop/MainActivity.java
+++ app/src/main/java/com/example/coffeeshop/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.coffeeshop;
 
 import android.os.Bundle;
 
+import com.example.coffeeshop.model.CartItem;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,41 +12,64 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.coffeeshop.databinding.ActivityMainBinding;
+import java.util.List;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    private String userName; // Armazena o nome do usuário
-    private String userEmail; // Armazena o email do usuário
+    private String userName;
+    private String userEmail;
+    private List<CartItem> cartItems; // lista itens carrinho
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Configurar o layout
+        // layout
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Capturar os dados do usuário enviados pela LoginActivity
+        // lista do carrinho
+        cartItems = new ArrayList<>();
+
+        // dados do usuário - LoginActivity
         userName = getIntent().getStringExtra("USER_NAME");
         userEmail = getIntent().getStringExtra("USER_EMAIL");
 
-        // Configurar BottomNavigationView
+        // BottomNavigationView
         BottomNavigationView navView = findViewById(R.id.nav_view);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_shop) //R.id.navigation_cart, R.id.navigation_orders - quando tiver
+                R.id.navigation_shop, R.id.navigation_cart, R.id.navigation_order) //  carrinho e pedidos
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
-    // Métodos para fornecer os dados do usuário aos fragmentos
+    // usuário aos fragmentos
     public String getUserName() {
         return userName;
     }
 
     public String getUserEmail() {
         return userEmail;
+    }
+
+    // manipular carrinho
+    public List<CartItem> getCartItems() {
+        return cartItems;
+    }
+
+    public void addItemToCart(CartItem item) {
+        cartItems.add(item);
+    }
+
+    public void removeItemFromCart(CartItem item) {
+        cartItems.remove(item);
+    }
+
+    public void clearCart() {
+        cartItems.clear();
     }
 }
---
+++ app/src/main/java/com/example/coffeeshop/model/CartItem.java
@@ -0,0 +1,31 @@
+package com.example.coffeeshop.model;
+
+public class CartItem {
+    private Product product; // relaciona o CartItem com um Product
+    private int quantity;
+
+    public CartItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getTotalPrice() {
+        return product.getPrice() * quantity; // preço total do item no carrinho
+    }
+}
