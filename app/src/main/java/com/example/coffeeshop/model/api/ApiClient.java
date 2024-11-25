5e8e7c3ad7fac0473b2799710fbf6faed2658a3f
Julia Retore <juliaretore@hotmail.com>
2024-11-24 21:40:09 -0300

    fixes

--- app/src/main/java/com/example/coffeeshop/model/api/ApiClient.java
+++ app/src/main/java/com/example/coffeeshop/model/api/ApiClient.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
     //private static final String BASE_URL = "https://coffeeshop-wrs1.onrender.com/";
-    private static final String BASE_URL = "http://192.168.1.102:8000/"; //para testes e implementacao
+    private static final String BASE_URL = "http://10.0.0.2:8000/"; //para testes e implementacao
 
     private static Retrofit retrofit;
 
